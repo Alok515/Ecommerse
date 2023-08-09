c47671b95e66112929b3793d462cfa69c644cf90
batman <kumarsinghalok28@gmail.com>
2023-08-09 12:15:22 +0530

    change to async

--- models/productSchema.js
+++ models/productSchema.js
@@ -1,9 +1,29 @@
 const mongoose = require('mongoose');
 
+const counterSchema = mongoose.Schema({
+    _id: {
+        type: String,
+        required: true,
+    },
+    seq: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+});
+
+const Counter = mongoose.model('Counter', counterSchema);
+
+const generateCount = async () => {
+    const count = await Counter.findOneAndUpdate({ _id: 'productId' }, { $inc: { seq: 1 } }, { new: true });
+    
+    return count.seq;
+}
+
 const productSchema = mongoose.Schema({
 
     id : {
-        type: Number,
+        type: String,
     },
     name: {
         type: String,
@@ -14,6 +34,18 @@ const productSchema = mongoose.Schema({
         required: true,
     },
 });
+
+productSchema.pre('save', async (next) => {
+    try {
+        const doc = this;
+        const count = await generateCount('productId');
+        console.log(count);
+        this.id = count;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+})
 
 const Product = mongoose.model('Product', productSchema);
 
